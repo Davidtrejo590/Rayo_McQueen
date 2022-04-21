52e9199a46784e08a91206ccda7e1c4a36b4b120
davidtrejo <luisdavidtorrestrejo@gmail.com>
2022-04-21 09:28:22 -0500

    initial commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/davidtrejo/Rayo_McQueen/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/davidtrejo/Rayo_McQueen/catkin_ws/devel;/home/davidtrejo/Webots-ROS/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/davidtrejo/Rayo_McQueen/catkin_ws/devel;/home/davidtrejo/Webots-ROS/catkin_ws/devel;/opt/ros/melodic' != '' else []
